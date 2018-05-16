ff1156f078456f041b04aedd373b7b273f03eb0e
Vitaliy Parshin <vitol_pvv@mail.ru>
2018-05-17 01:03:41 +0300

    Тесты: - добавил тестов протокола

--- jim/test_jim.py
+++ jim/test_jim.py
@@ -1,7 +1,7 @@
 import jim.protocol as protocol
 
 
-class TestJim:
+class TestProtocol:
 
     def test_get_code_description(self):
         # описание кода по константе
@@ -15,3 +15,42 @@ class TestJim:
 
         # описание не существующего кода
         assert protocol.Code.get_description(123456) is None
+
+    def test_message_encoding_change(self):
+        # кодировка по-умолчанию
+        default_encoding = protocol.Message.get_encoding()
+
+        assert protocol.Message.get_encoding() == 'utf-8'
+
+        # смена кодировки
+        protocol.Message.set_encoding('ascii')
+        assert protocol.Message.get_encoding() == 'ascii'
+
+        # смена кодировки
+        protocol.Message.set_encoding(default_encoding)
+        assert protocol.Message.get_encoding() == 'utf-8'
+
+    def test_request_presence(self):
+        message = protocol.Message.request_presence()
+
+        # сообщение содержит поле 'action'
+        assert protocol.Message.parse(message).get(protocol.Message.KEY_ACTION)
+        # поле 'action' == 'presence'
+        assert protocol.Message.parse(message).get(protocol.Message.KEY_ACTION) == protocol.Message.ACTION_PRESENCE
+
+    def test_response(self):
+        code = protocol.Code.C_200
+        message = protocol.Message.response(code)
+
+        # сообщение содержит поле 'response'
+        assert protocol.Message.parse(message).get(protocol.Message.KEY_RESPONSE)
+        # поле 'response' == code(200)
+        assert protocol.Message.parse(message).get(protocol.Message.KEY_RESPONSE) == code
+
+    def test_parse(self):
+        message = protocol.Message.request_presence()
+
+        # протокол возврыщает сообщение в виде набора байт
+        assert type(message) == bytes
+        # после парсинга получаем словарь
+        assert type(protocol.Message.parse(message)) == dict
